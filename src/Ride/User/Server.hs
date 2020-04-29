66447346991da8ad3d892f4f911951d3fee9bb9f
Fabio Luz <fabio.luz@outlook.com>
2020-04-28 22:40:42 -0300

    fix status

--- src/Ride/User/Server.hs
+++ src/Ride/User/Server.hs
@@ -13,12 +13,13 @@ import Servant
   , PostCreated 
   , ReqBody
   , ServerT
+  , NoContent (..)
   , (:>)
   , (:<|>) (..)
-  , err401
+  , err403
   )
 import Servant.Auth.Server (AuthResult (..), throwAll)
-import Ride.App (AppT, logInfo)
+import Ride.App (AppT)
 import Ride.Auth.Class (LoggedInUser (..))
 import Ride.Error (orThrow422, validationError)
 import Ride.Shared.Types (Id (..))
@@ -34,24 +35,25 @@ import Ride.User.Class
 
 import qualified Ride.User.DB as DB
 
-type UserAPI = "me" :> Get '[JSON] User
-          :<|> "users" :> Get '[JSON] [User]
-          :<|> "users" :> ReqBody '[JSON] CreateUser :> PostCreated '[JSON] User
-          :<|> "users" :> Capture "userId" (Id User) :> ReqBody '[JSON] UpdateUser :> Put '[JSON] ()
+type UserAPI
+    =  "me" :> Get '[JSON] User
+  :<|> "users" :> Get '[JSON] [User]
+  :<|> "users" :> ReqBody '[JSON] CreateUser :> PostCreated '[JSON] User
+  :<|> "users" :> Capture "userId" (Id User) :> ReqBody '[JSON] UpdateUser :> Put '[JSON] NoContent
 
 userServer :: (MonadIO m) => AuthResult LoggedInUser -> ServerT UserAPI (AppT m)
 userServer (Authenticated user)
-     = getMeHandler user
+    =  getMeHandler user
   :<|> getUsersHandler
   :<|> createUserHandler
   :<|> updateUserHandler
 
-userServer _ = throwAll err401
+userServer _ = throwAll err403
 
 getMeHandler :: MonadIO m => LoggedInUser -> AppT m User
 getMeHandler user = do
   userRes <- DB.getUserById $ userId user
-  maybe (throwIO err401) pure userRes
+  maybe (throwIO err403) pure userRes
 
 getUsersHandler :: MonadIO m => AppT m [User]
 getUsersHandler = DB.getAllUsers
@@ -64,8 +66,8 @@ createUserHandler input = do
   DB.insertUser user password
   pure user
 
-updateUserHandler :: MonadIO m => Id User -> UpdateUser -> AppT m ()
+updateUserHandler :: MonadIO m => Id User -> UpdateUser -> AppT m NoContent
 updateUserHandler userId input = do
   user <- updateUser userId input `orThrow422` validationError
   DB.updateUser user
-  pure ()
+  pure NoContent
